fa3bd221fd0dfa5b461962d9deeeeaff7583c5a9
Sudan <sudan@pop-os.localdomain>
2020-11-28 18:28:43 +0545

    fetching django rest framework api

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,36 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
-class App extends Component{
-  constructor(){
+class App extends Component {
+  constructor() {
     super();
-    this.state={
-      friend:[
-        {
-          'name':'sameep'
-          
-        },
-        {
-          'name':'himal'
-        },
-        {
-          'name':'khem'
-        },
-      ]
+    this.state = {
+      friend: []
     };
   }
+  componentDidMount() {
+    fetch('http://127.0.0.1:8000/viewset/studentmodel/')
+      .then(response => response.json())
+      .then(user => this.setState({ friend: user }));
 
-  render(){
-    return(
+  }
+
+  render() {
+    return (
       <div className="App">
-        { this.state.friend.map(friend=><h3>{ friend.name}</h3>)}
-     
-    </div>
-    
+        { this.state.friend.map(friend => <h3 key={friend.id}>{friend.first_name}</h3>)}
+
+      </div>
+
     )
   }
 }
 
-  
+
 
 export default App;
